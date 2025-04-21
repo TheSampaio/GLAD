88535d60d05481411527db5841fff8e248cc785c
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-04-21 01:25:43 -0300

    Build System Improved

--- premake5.lua
+++ premake5.lua
@@ -1,17 +1,30 @@
-project "glad"
+    project "glad"
     kind "StaticLib"
     language "C"
-    location "build"
+    staticruntime "On"
+    
+    targetdir (".Out/Bin/" .. output_dir .. "%{prj.name}")
+    objdir    (".Out/Obj/" .. output_dir .. "%{prj.name}")
 
-    targetdir ("out/build/bin/%{cfg.buildcfg}/%{prj.name}")
-    objdir    ("out/build/obj/%{cfg.buildcfg}/%{prj.name}")
+    files
+    {
+        "include/glad/glad.h",
+        "include/KHR/khrplatform.h",
+        "source/glad.c"
+    }
 
-    includedirs { "include" }
-    files { "source/glad.c" }
-
+    includedirs
+    {
+        "include"
+    }
+    
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
